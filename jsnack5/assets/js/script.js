42484b70eba24b5cb653a843f176b56d8b4a7d87
Francesco <franc.ns@gmail.com>
2019-10-25 14:28:25 +0200

    added jsnack5

---
+++ jsnack5/assets/js/script.js
@@ -0,0 +1,15 @@
+// Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
+
+// creazione variabili
+var i, list = [], num;
+
+// ciclo con input utente e controllo dispari
+for (i = 0; i < 6; i++) {
+  num = parseInt(prompt('Inserisci un numero'));
+  if (num % 2 !== 0) {
+    list.push(num);
+  }
+}
+
+// output
+console.log(list);
